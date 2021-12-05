d401b78c13f1ea2081baea6c5dffeb476cb0f7de
KingCh1ll <77991729+KingCh1ll@users.noreply.github.com>
2021-12-04 23:27:48 -0500

    🚀 | `guildCreate` Msg - Added Section on Chat Bot

--- bot/events/guildCreate.js
+++ bot/events/guildCreate.js
@@ -48,7 +48,7 @@ module.exports = {
 
 			await guild.systemChannel.send({
 				content:
-					"Hi! My name's SparkV. I'm a powerful multipurpose meme/chat bot with over 100+ commands to keep your server entertained and active! All, without spending a dime. Simply use the command ^Help to get a list of my commands. Want to enable a setting? Go to my dashboard! Use the command ^Dashboard and click on the link I send you. Thanks for inviting me!\n\nPsst... don't froget to review me on top.gg!",
+					"Hi! My name's SparkV. I'm a powerful multipurpose meme/chat bot with over 100+ commands to keep your server entertained and active! All, without spending a dime. Simply use the command ^Help to get a list of my commands. Want to enable a setting? Go to my dashboard! Use the command ^Dashboard and click on the link I send you. Thanks for inviting me!\n\nPsst... don't froget to review me on top.gg! Oh, and also mention me (if chatbot is enable) to talk to me!",
 				components: [new Discord.MessageActionRow().addComponents(InviteButton, SupportButton, VoteButton)],
 			}).catch(err => console.log(`Failed to send message to ${guild.name} (${guild.id})! ${err.message}`));
 		}
