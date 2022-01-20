372a082cb2b07a6f15300cfb850d2d7be0c556b7
KingCh1ll <77991729+KingCh1ll@users.noreply.github.com>
2022-01-20 11:26:51 -0500

    🚀 | Created NEW Configuration Category!

---
+++ bot/commands/Configuration/index.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+
+const commands = fs
+	.readdirSync(__dirname)
+	.filter(c => c !== "index.js")
+	.map(c => require(`${__dirname}/${c}`));
+
+module.exports = {
+	name: "Configuration",
+	description: "These commands let you personalize SparkV!",
+	emoji: "⚙️",
+	commands,
+};
