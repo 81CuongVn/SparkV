845d297a224dc8c78e0a0fb6d593a68cbd39499c
KingCh1ll <77991729+KingCh1ll@users.noreply.github.com>
2022-01-24 00:45:57 -0500

    🐛 | Fixed Set Coins

--- bot/commands/Owner/setcoins.js
+++ bot/commands/Owner/setcoins.js
@@ -3,15 +3,14 @@ const Discord = require(`discord.js`);
 const cmd = require("../../templates/command");
 
 async function execute(bot, message, args, command, data) {
-	const User = await bot.functions.GetMember(message, args);
+	const User = message?.applicationId ? data.options.getMember("user") : await bot.functions.fetchUser(args[0]);
+	const UserData = await bot.database.getUser(User.id);
 
-	if (!User) {
-		return await message.replyT;
-	}
+	if (!User) return await message.replyT("Unable to fetch user. Please try again later.");
 
-	data.user.money.balance = args[1];
-	data.user.markModified("money.balance");
-	await data.user.save();
+	UserData.money.balance = message?.applicationId ? data.options.getString("coins") : args[1];
+	UserData.markModified("money.balance");
+	await UserData.save();
 
 	await message.replyT(`${bot.config.emojis.success} | Success!`);
 }
@@ -21,5 +20,18 @@ module.exports = new cmd(execute, {
 	aliases: [],
 	dirname: __dirname,
 	usage: `(user) <ammount>`,
-	ownerOnly: true
+	ownerOnly: true,
+	slash: true,
+	options: [
+		{
+			type: 6,
+			name: "user",
+			description: "The user to set the coins of.",
+		},
+		{
+			type: 3,
+			name: "coins",
+			description: "The amount of coins this use should have."
+		}
+	]
 });
