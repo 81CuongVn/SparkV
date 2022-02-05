bc691d9208bce09e7adc3dd48a5e252af3443808
KingCh1ll <77991729+KingCh1ll@users.noreply.github.com>
2022-02-04 20:29:49 -0500

    ⚒️ | Removed Unneeded Package

--- web.js
+++ web.js
@@ -11,11 +11,5 @@ const app = express();
 
 // Code //
 console.log("-------- Website --------");
-app.use(
-	require("serve-favicon")(
-		path.resolve(`${process.cwd()}${path.sep}assets${path.sep}images${path.sep}site${path.sep}favicon.ico`),
-	),
-);
-
 app.use("*", (req, res) => res.status(200).send({ status: 200, message: "SparkV is online." }));
 app.listen(process.env.PORT || 3000, () => console.log("💻 | Server listening to port 3000."));
