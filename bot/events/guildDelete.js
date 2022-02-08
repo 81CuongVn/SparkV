06664f1f2daea12f0c103075487bf3207ad1c175
KingCh1ll <77991729+KingCh1ll@users.noreply.github.com>
2022-02-08 15:50:25 -0500

    🔧 | Updated Logger For GuildDelete

--- bot/events/guildDelete.js
+++ bot/events/guildDelete.js
@@ -20,14 +20,30 @@ module.exports = {
 		});
 
 		const Logger = bot.channels.cache.get("831314946624454656");
+		const Owner = await guild?.fetchOwner() || null;
 
 		if (Logger) {
 			const ServerRemovedEmbed = new Discord.MessageEmbed()
 				.setTitle("🔽︱Guild Removed")
 				.setDescription(`SparkV left **${guild.name} (${guild.id})**.`)
+				.addField("<:player:933552618272350249> **Members**", `${bot.functions.formatNumber(guild.memberCount)}`, true)
+				.addField("📅 **Created**", `<t:${~~(guild.createdAt / 1000)}:R>`, true)
 				.setThumbnail(guild.iconURL())
 				.setImage(guild.bannerURL())
 				.setColor("RED");
+
+			if (guild.vanityURLCode) {
+				ServerAddedEmbed
+					.setURL(`https://discord.gg/${guild.vanityURLCode}`)
+					.addField("🔗 **Vanity URL**", `https://discord.gg/${guild.vanityURLCode}`, true);
+			}
+
+			if (Owner) {
+				ServerAddedEmbed.setAuthor({
+					name: Owner?.user?.username,
+					iconURL: Owner?.user?.displayAvatarURL({ dynamic: true })
+				});
+			}
 
 			Logger.send({
 				embeds: [ServerRemovedEmbed],
