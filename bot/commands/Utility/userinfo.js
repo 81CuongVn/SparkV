5718374c96879ce29095105a049fdea8230ed91f
KingCh1ll <77991729+KingCh1ll@users.noreply.github.com>
2022-02-10 16:25:57 -0500

    👀 | More Info/UI

--- bot/commands/Utility/userinfo.js
+++ bot/commands/Utility/userinfo.js
@@ -10,10 +10,19 @@ const statuses = {
 	offline: "⚫"
 };
 
+const badges = {
+	HOUSE_BRAVERY: "<:house_bravery:941439956528803860>",
+	HOUSE_BRILLIANCE: "<:house_brilliance:941439956507840543>",
+	HOUSE_BALANCE: "<:house_balance:941440238486691921>",
+};
+
 module.exports = new cmd(
 	async (bot, message, args, command, data) => {
 		let user = message?.applicationId ? data.options.getMember("user") || message.user : (await bot.functions.fetchUser(args[0]) || message.author);
 		const member = message.channel.guild.members.cache.get(user.id);
+
+		const userdata = await bot.database.getUser(user.id);
+		// const roles = member.roles.cache.map(r => `<@&${r.id}>`).join(", ");
 
 		user = user.user ? await user.user.fetch() : await user.fetch();
 
@@ -38,15 +47,19 @@ module.exports = new cmd(
 			.addField(`\`${statuses[member?.presence?.status || "offline"]}\` Status`, `${member?.presence?.status || "offline"}`, true)
 			.addField("\`⌚\` Joined Server", `<t:${~~(member.joinedAt / 1000)}:R>`, true)
 			.addField("\`🚪\` Registered", `<t:${~~(user.createdAt / 1000)}:R>`, true)
+			.addField("\`😎\` Using SparkV Since", `<t:${~~(userdata.registrationDate / 1000)}:R>`, true)
 			.addField("\`🔢\`Join Position", `${await position || "UNKNOWN"}/${members.length}`, true)
+			// .addField("\`🔧\` Roles", roles || "None", false)
 			.setFooter({
-				text: `\`🔢\` ID: ${user.user ? user.user.id : user.id} • ${bot.config.embed.footer}`,
+				text: `🔢 ID: ${user.user ? user.user.id : user.id} • ${bot.config.embed.footer}`,
 				iconURL: bot.user.displayAvatarURL({ dynamic: true })
 			})
 			.setColor(bot.config.embed.color);
 
-		if (user?.flags?.length > 0) InfoEmbed.addField("\`🏅\`Badges", `${user.flags.toArray().map(b => b)}`, true);
-		if (user.user ? user.user.bannerURL({ dynamic: true }) : user.bannerURL({ dynamic: true })) InfoEmbed.setImage(user.user ? user.user.bannerURL({ dynamic: true, size: 1024 }) : user.bannerURL({ dynamic: true, size: 1024 }));
+		const userflags = user.user ? await user.user.fetchFlags() : await user.fetchFlags();
+
+		if (userflags) InfoEmbed.addField("\`🏅\`Badges", `${user.flags.toArray().map(b => badges[b] ? badges[b] : b)}`, true);
+		if (user.user ? user.user.banner : user.banner) InfoEmbed.setImage(user.user ? user.user.bannerURL({ dynamic: true, size: 1024 }) : user.bannerURL({ dynamic: true, size: 1024 }));
 
 		await message.replyT({
 			embeds: [InfoEmbed],
