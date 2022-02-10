337906eedd2a8035bc059afb21a9cba58b368916
KingCh1ll <77991729+KingCh1ll@users.noreply.github.com>
2022-02-09 21:26:18 -0500

    👀 | Join Position + Badges Added | UserInfo Cmd

--- bot/commands/Utility/userinfo.js
+++ bot/commands/Utility/userinfo.js
@@ -10,6 +10,16 @@ module.exports = new cmd(
 
 		user = user.user ? await user.user.fetch() : await user.fetch();
 
+		const members = message.guild.members.cache
+			.sort((a, b) => a.joinedTimestamp - b.joinedTimestamp)
+			.toJSON();
+
+		const position = new Promise(forfill => {
+			for (let i = 1; i < members.length + 1; i++) {
+				if (members[i - 1].id === member.id) forfill(i);
+			}
+		});
+
 		const InfoEmbed = new Discord.MessageEmbed()
 			.setAuthor({
 				name: user.user ? user.user.tag : user.tag,
@@ -18,20 +28,18 @@ module.exports = new cmd(
 					: user.displayAvatarURL({ dynamic: true }),
 			})
 			.setThumbnail(user.user ? user.user.displayAvatarURL({ dynamic: true }) : user.displayAvatarURL({ dynamic: true }))
-			.addField("**JOINED**", `<t:${~~(member.joinedAt / 1000)}:R>`, true)
-			.addField("**REGISTERED**", `<t:${~~(user.createdAt / 1000)}:R>`, true)
-			.addField("**AVATAR**", `[Click for Avatar](${user.user ? user.user.displayAvatarURL({ dynamic: true }) : user.displayAvatarURL({ dynamic: true })})`, true)
+			.addField("Status", `${member?.presence?.status || "offline"}`, true)
+			.addField("Joined", `<t:${~~(member.joinedAt / 1000)}:R>`, true)
+			.addField("Registered", `<t:${~~(user.createdAt / 1000)}:R>`, true)
+			.addField("Join Position", `${await position || "UNKNOWN"}/${members.length}`, true)
+			.addField("Badges", `${user.flags.toArray().map(b => b)}`, true)
 			.setFooter({
 				text: `ID: ${user.user ? user.user.id : user.id} • ${bot.config.embed.footer}`,
 				iconURL: bot.user.displayAvatarURL({ dynamic: true })
 			})
 			.setColor(bot.config.embed.color);
 
-		if (user.user ? user.user.bannerURL({ dynamic: true }) : user.bannerURL({ dynamic: true })) {
-			InfoEmbed
-				.setImage(user.user ? user.user.bannerURL({ dynamic: true, size: 1024 }) : user.bannerURL({ dynamic: true, size: 1024 }))
-				.addField("**BANNER**", `[Click for Banner](${user.user ? user.user.displayAvatarURL({ dynamic: true }) : user.displayAvatarURL({ dynamic: true })})`, true);
-		}
+		if (user.user ? user.user.bannerURL({ dynamic: true }) : user.bannerURL({ dynamic: true })) InfoEmbed.setImage(user.user ? user.user.bannerURL({ dynamic: true, size: 1024 }) : user.bannerURL({ dynamic: true, size: 1024 }));
 
 		await message.replyT({
 			embeds: [InfoEmbed],
