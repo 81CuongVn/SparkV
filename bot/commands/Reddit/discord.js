eab6452d971caf6ef034f75e54393086c3168f81
KingCh1ll <77991729+KingCh1ll@users.noreply.github.com>
2022-02-14 21:49:45 -0500

    🎮 | Added Discord Reddit Command

---
+++ bot/commands/Reddit/discord.js
@@ -0,0 +1,11 @@
+const RedditCommand = require("../../templates/redditCommand");
+
+module.exports = new RedditCommand({
+	description: "See what's going on on the Discord reddit!",
+	dirname: __dirname,
+	aliases: ["dailycord", "disdaily"],
+	usage: "",
+	enabled: true,
+	endpoint: "/r/discordapp/top/.json?sort=top&t=week",
+	type: "image",
+});
