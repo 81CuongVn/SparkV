2db2d7e5e115686b47fc18476edb9935ec54cf27
KingCh1ll <77991729+KingCh1ll@users.noreply.github.com>
2022-02-25 20:19:48 -0500

    🔵 | Fixed Mod Command Perms

--- bot/templates/modCommand.js
+++ bot/templates/modCommand.js
@@ -8,29 +8,6 @@ module.exports = class ModCommand {
 	}
 
 	async run(bot, message, args, command, data) {
-		for (const requiredPerm of this.settings.perms || []) {
-			if (!message.channel.permissionsFor(message.member).has(requiredPerm)) {
-				return await message.replyT(this.missingPermission("user", requiredPerm));
-			} else if (!message.channel.permissionsFor(message.guild.me).has(requiredPerm)) {
-				return await message.replyT(this.missingPermission("bot", requiredPerm));
-			}
-		}
-
 		return this.execute(bot, message, args, command, data);
-	}
-
-	missingPermission(type, permission) {
-		const permissions = {
-			KICK_MEMBERS: "kick members",
-			BAN_MEMBERS: "ban members",
-			MANAGE_CHANNELS: "manage and edit channels",
-			MANAGE_GUILD: "manage and edit server settings",
-			MANAGE_MESSAGES: "manage and remove messages",
-			MANAGE_NICKNAMES: "edit other people's nicknames",
-			MANAGE_ROLES: "manage the roles on this server",
-			MODERATE_MEMBERS: "timeout members",
-		};
-
-		return `Sorry, ${type === "bot" ? "i'm" : "you're"} missing the \`${permission.toUpperCase()}\` permission.\nMake sure ${type === "bot" ? "I have" : "you have"} access to **${permissions[permission]}** and try again.`;
 	}
 };
