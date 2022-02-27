f785073f460ed01227469c61da0ae19bf2978fcb
KingCh1ll <77991729+KingCh1ll@users.noreply.github.com>
2022-02-26 19:11:04 -0500

    🌐 | Updated Database Schema for Roles Update

--- database/schemas/guild.js
+++ database/schemas/guild.js
@@ -12,60 +12,38 @@ const Schema = new mongoose.Schema({
 	casesCount: { type: Number, default: 0 },
 	autoRemoveCommands: { type: Boolean, default: "false" },
 
-	plugins: {
-		logging: {
-			type: Object,
-			default: {
-				enabled: "false",
-				channel: null,
-				level: "info"
-			}
-		},
+	plugins: { type: Object, default: {
 		welcome: {
-			type: Object,
-			default: {
-				enabled: "false",
-				message: "Welcome {mention} to **{server}**! You're our **{members}th member**!",
-				channel: null
-			}
+			enabled: "false",
+			message: "Welcome {mention} to **{server}**! You're our **{members}th member**!",
+			roles: [],
+			channel: null
 		},
 		goodbye: {
-			type: Object,
-			default: {
-				enabled: "false",
-				message: "Bye {mention}! We're really sad to see you go. Without you, we're now **{members} members**.",
-				channel: null
-			}
+			enabled: "false",
+			message: "Bye {mention}! We're really sad to see you go. Without you, we're now **{members} members**.",
+			channel: null
 		},
 		automod: {
-			type: Object,
-			default: {
-				removeLinks: "false",
-				removeProfanity: "false",
-				removeDuplicateText: "false"
-			}
+			removeLinks: "false",
+			removeProfanity: "false",
+			removeDuplicateText: "false"
 		},
 		leveling: {
-			type: Object,
-			default: {
-				enabled: "false",
-				message: "<a:tada:819934065414242344> Congrats {author}, you're now at level **{level}**!",
-				channel: null,
-				max: 25,
-				min: 5
-			}
+			enabled: "false",
+			message: "<a:tada:819934065414242344> Congrats {author}, you're now at level **{level}**!",
+			channel: null,
+			max: 25,
+			min: 5
 		},
 		starboard: {
-			type: Object,
-			default: {
-				enabled: "false",
-				channel: null,
-				emoji: "⭐",
-				min: 1,
-			}
+			enabled: "false",
+			channel: null,
+			emoji: "⭐",
+			min: 1,
 		},
-		chatbot: { type: String, default: "false" },
-	}
+		chatbot: "false",
+	} }
 });
 
 module.exports = new mongoose.model("Guild", Schema);
