89e14ef75f46424d43f16745528d8928149a5c11
KingCh1ll <77991729+KingCh1ll@users.noreply.github.com>
2022-03-06 13:11:53 -0500

    🔨 | Fixed UselessFact

--- bot/commands/Fun/uselessfact.js
+++ bot/commands/Fun/uselessfact.js
@@ -4,27 +4,27 @@ const request = require("axios");
 const cmd = require("../../templates/command");
 
 async function execute(bot, message) {
-	request.get("https://uselessfacts.jsph.pl/random.response?language=en")
-		.then(async response => {
-			const FunFactEmbed = new Discord.MessageEmbed()
-				.setTitle(`${bot.config.emojis.success} | Did you know?`)
-				.setDescription(response.data.text)
-				.setFooter({
-					text: `Fun facts powered by https://uselessfacts.jsph.pl! • ${bot.config.embed.footer}`,
-					iconURL: bot.user.displayAvatarURL({ dynamic: true })
-				})
-				.setColor(bot.config.embed.color)
-				.setTimestamp();
+	request.get("https://uselessfacts.jsph.pl/random.json?language=en").then(async response => {
+		const FunFactEmbed = new Discord.MessageEmbed()
+			.setTitle(`${bot.config.emojis.success} | Did you know?`)
+			.setDescription(response.data.text)
+			.setFooter({
+				text: `Fun facts powered by https://uselessfacts.jsph.pl! • ${bot.config.embed.footer}`,
+				iconURL: bot.user.displayAvatarURL({ dynamic: true })
+			})
+			.setColor(bot.config.embed.color)
+			.setTimestamp();
 
-			await message.replyT({
-				embeds: [FunFactEmbed],
-			});
+		await message.replyT({
+			embeds: [FunFactEmbed],
 		});
+	});
 }
 
 module.exports = new cmd(execute, {
 	description: "I will get a useless fact! You're better off with the advice command...",
 	aliases: ["uf"],
 	dirname: __dirname,
-	usage: `<word>`,
+	usage: "",
+	slash: true
 });
