eb94d630a6bfc9e721a22cee6c6a2d7f7778b465
KingCh1ll <77991729+KingCh1ll@users.noreply.github.com>
2022-03-13 18:12:53 -0400

    🚀 | Permissions Fixed

--- bot/templates/modCommand.js
+++ bot/templates/modCommand.js
@@ -8,6 +8,28 @@ module.exports = class ModCommand {
 	}
 
 	async run(bot, message, args, command, data) {
+		const perms = message.channel.permissionsFor(message.user ? message.user : message.author);
+
+		for (const perm of this.settings.perms) {
+			if (!perms.has(discord.Permissions.FLAGS[perm])) {
+				return await message.replyT({
+					content: `${bot.config.emojis.error} | Uh oh! You're missing the \`${perm}\` permission!`,
+					ephemeral: true,
+				});
+			}
+		}
+
+		const botperms = message.channel.permissionsFor(message.guild.me);
+
+		for (const perm of this.settings.bot_perms) {
+			if (!botperms.has(discord.Permissions.FLAGS[perm])) {
+				return await message.replyT({
+					content: `${bot.config.emojis.error} | Uh oh! I'm missing the \`${perm}\` permission!`,
+					ephemeral: true,
+				});
+			}
+		}
+
 		return this.execute(bot, message, args, command, data);
 	}
 };
