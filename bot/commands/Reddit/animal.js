844667efc7daf91a344b8e53407a33c17f2f13b6
KingCh1ll <77991729+KingCh1ll@users.noreply.github.com>
2022-03-13 23:59:37 -0400

    🐶 | Animal Commands - Now Only One!

--- bot/commands/Reddit/animal.js
+++ bot/commands/Reddit/animal.js
@@ -8,4 +8,38 @@ module.exports = new RedditCommand({
 	enabled: true,
 	endpoint: "/r/aww/top/.json?sort=top&t=week",
 	type: "image",
+	slash: true,
+	options: [
+		{
+			type: 3,
+			name: "type",
+			description: "The type of animal.",
+			choices: [
+				{
+					name: "bunnies",
+					value: "/r/bunnies/top/.json?sort=top&t=week"
+				},
+				{
+					name: "dogs",
+					value: "https://www.reddit.com/r/dogpictures/top/.json?sort=top&t=week"
+				},
+				{
+					name: "cats",
+					value: "/r/catpictures/top/.json?sort=top&t=day"
+				},
+				{
+					name: "ducks",
+					value: "/r/Duckpictures/top/.json?sort=top&t=day"
+				},
+				{
+					name: "foxes",
+					value: "/r/foxes/top/.json?sort=top&t=week"
+				},
+				{
+					name: "monkeys",
+					value: "/r/monkeys/top/.json?sort=top&t=week"
+				}
+			]
+		}
+	],
 });
