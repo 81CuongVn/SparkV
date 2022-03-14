90d569390f337fbe46b1e78697fc06d26104c24d
KingCh1ll <77991729+KingCh1ll@users.noreply.github.com>
2022-03-14 19:40:42 -0400

    🚀 | Added Required Perm

--- bot/commands/Utility/roleall.js
+++ bot/commands/Utility/roleall.js
@@ -4,21 +4,25 @@ const canvacord = require("canvacord");
 const cmd = require("../../templates/command");
 
 async function execute(bot, message, args, command, data) {
-	const role = message?.applicationId ? data.options.getRole("role") : message.mentions.roles.first();
+	const role = data.options.getRole("role");
 
-	if (!role) return message.editT("Please mention a valid role to get the id of.");
+	try {
+		await message.guild.members.cache.forEach(async member => await member.roles.add(role));
 
-	await message.guild.members.cache.forEach(async member => await member.roles.add(role));
-
-	await message.editT(`Successfully added the role **${role}** to **${message.guild.members.cache.size}** members.`);
+		await message.editT(`Successfully added the role **${role}** to **${message.guild.members.cache.size}** members.`);
+	} catch (err) {
+		await message.editT(`Failed to give **${role}**. Please check my permissions and try again.`);
+	}
 }
 
 module.exports = new cmd(execute, {
 	description: "Give all users a role.",
 	dirname: __dirname,
 	aliases: ["rall"],
+	perms: ["MANAGE_ROLES"],
 	usage: `(@role)`,
 	slash: true,
+	slashOnly: true,
 	ephemeral: true,
 	options: [
 		{
