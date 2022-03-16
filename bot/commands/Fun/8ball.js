862af0af66c6be1628591d4f4853258a34d3177f
KingCh1ll <77991729+KingCh1ll@users.noreply.github.com>
2022-03-15 20:54:40 -0400

    🚀 | Changed Strings

--- bot/commands/Fun/8ball.js
+++ bot/commands/Fun/8ball.js
@@ -1,36 +1,35 @@
-const Discord = require(`discord.js`);
+const Discord = require("discord.js");
 
 const cmd = require("../../templates/command");
 
 const Replies = [
-	`It is certain`,
-	`It is decidedly so`,
-	`Without a doubt`,
-	`Yes, definitely`,
-	`You may rely on it`,
-	`As I see it, yes`,
-	`Most likely`,
-	`Outlook good`,
-	`Yes`,
-	`Signs pointing to yes`,
-	`Ask again later`,
-	`Better not tell you now`,
-	`Cannot predict now`,
-	`Concentrate and ask again`,
-	`Don't count on it`,
-	`My reply is no`,
-	`My sources say no`,
-	`Outlook not so good`,
-	`very doubtful`,
+	"It is certain",
+	"It is decidedly so",
+	"Without a doubt",
+	"Yes, definitely",
+	"You may rely on it",
+	"As I see it, yes",
+	"Most likely",
+	"Outlook good",
+	"Yes",
+	"Signs pointing to yes",
+	"Ask again later",
+	"Better not tell you now",
+	"Cannot predict now",
+	"Concentrate and ask again",
+	"Don't count on it",
+	"My reply is no",
+	"My sources say no",
+	"Outlook not so good",
+	"very doubtful",
 ];
 
 module.exports = new cmd(async (bot, message, args, command, data) => await message.replyT(Replies[Math.floor(Math.random() * Replies.length + 0)]), {
-	description: `Just a little fun.`,
+	description: "Just a little fun.",
 	dirname: __dirname,
 	aliases: ["ball"],
-	usage: `(question)`,
+	usage: "(question)",
 	slash: true,
-	slashOnly: true,
 	options: [
 		{
 			type: 3,
