ab9f29bf02d5dd0cea5163447925a5ac5850064d
KingCh1ll <77991729+KingCh1ll@users.noreply.github.com>
2022-03-19 22:54:45 -0400

    ♻️ | Encode Cmd - Now Slash Enabled!

--- bot/commands/Utility/encode.js
+++ bot/commands/Utility/encode.js
@@ -2,28 +2,56 @@ const Discord = require("discord.js");
 
 const cmd = require("../../templates/command");
 
-module.exports = new cmd(
-	async (bot, message, args) => {
-		if (!args) return await message.replyT("Next time, choose the type of encoding and the text to encode. Types: `base64`, `hex` or `url`");
+async function execute(bot, message, args, command, data) {
+	const type = data.options.getString("type");
+	const string = data.options.getString("text");
 
-		const [type, ...string] = args;
+	if (type === "base64") {
+		await message.replyT(Buffer.from(string.join(" ")).toString("base64"));
+	} else if (type === "hex") {
+		const h = Buffer.from(string.join(" ")).toString("base64");
+		const e = Buffer.from(h, "base64");
 
-		if (type === "base64") {
-			await message.replyT(Buffer.from(string.join(" ")).toString("base64"));
-		} else if (type === "hex") {
-			const h = Buffer.from(string.join(" ")).toString("base64");
-			const e = Buffer.from(h, "base64");
+		await message.replyT(e.toString("hex"));
+	} else if (type === "url") {
+		await message.replyT(encodeURIComponent(string.join(" ")));
+	}
+}
 
-			await message.replyT(e.toString("hex"));
-		} else if (type === "url") {
-			await message.replyT(encodeURIComponent(string.join(" ")));
-		}
-	},
-	{
-		description: "encode a string",
-		dirname: __dirname,
-		usage: "<type> <string>",
-		aliases: [],
-		perms: []
-	},
-);
+module.exports = new cmd(execute, {
+	description: "Encode a string.",
+	dirname: __dirname,
+	usage: "(string) (type)",
+	aliases: [],
+	perms: [],
+	slash: true,
+	slashOnly: true,
+	options: [
+		{
+			type: 3,
+			name: "text",
+			description: "The text to encode.",
+			required: true
+		},
+		{
+			type: 3,
+			name: "type",
+			description: "The type of encoding to use. Types: `base64`, `hex` or `url`",
+			required: true,
+			choices: [
+				{
+					name: "base64",
+					value: "base64"
+				},
+				{
+					name: "hex",
+					value: "hex"
+				},
+				{
+					name: "url",
+					value: "url"
+				}
+			]
+		},
+	]
+});
