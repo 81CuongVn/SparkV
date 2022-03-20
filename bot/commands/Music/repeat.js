ffab6525d8c00ec3d42e088a23ab1d7a22eaae1a
KingCh1ll <77991729+KingCh1ll@users.noreply.github.com>
2022-03-20 08:16:46 -0400

    ♻️ | Repeat Cmd - Now Slash Enabled!

--- bot/commands/Music/repeat.js
+++ bot/commands/Music/repeat.js
@@ -4,20 +4,23 @@ const cmd = require("../../templates/musicCommand");
 
 async function execute(bot, message, args, command, data) {
 	const Queue = bot.distube.getQueue(message);
-	let mode;
 
 	if (!Queue) return await message.replyT(`${bot.config.emojis.error} | There is no music playing in this guild.`);
 
-	args[0] = args[0].toLowerCase();
+	const state = data.options.getString("state");
+	const type = data.options.getString("type");
+	let mode;
 
-	if (args[0] === `song`) mode = 1;
-	else if (args[0] === `queue`) mode = 2;
-	else if (args[0] === "off") mode = 0;
-	else return await message.replyT(`${bot.config.emojis.error} | Invalid argument.`);
+	if (state === "on") {
+		if (type === `song`) mode = 1;
+		else if (type === `queue`) mode = 2;
+	} else if (state === "off") {
+		mode = 0;
+	}
 
 	Queue.setRepeatMode(mode);
 
-	await message.replyT(`${bot.config.emojis.music} | Okay, I'll ${args[0] === "off" ? `stop the loop.` : `loop the ${args[0]}.`}`);
+	await message.replyT(`${bot.config.emojis.music} | Okay, I ${state === "off" ? `stopped the loop.` : `looped the ${type}.`}`);
 }
 
 module.exports = new cmd(execute, {
@@ -27,4 +30,40 @@ module.exports = new cmd(execute, {
 	aliases: ["replay", "loop"],
 	perms: [],
 	requireArgs: true,
+	slash: true,
+	slashOnly: true,
+	options: [
+		{
+			type: 3,
+			name: "state",
+			description: "Whether to repeat or not.",
+			required: true,
+			choices: [
+				{
+					name: "on",
+					value: "on"
+				},
+				{
+					name: "off",
+					value: "off"
+				}
+			]
+		},
+		{
+			type: 3,
+			name: "type",
+			description: "The repeating type. Song will repeat the song. Queue will repeat the queue.",
+			required: true,
+			choices: [
+				{
+					name: "song",
+					value: "song"
+				},
+				{
+					name: "queue",
+					value: "queue"
+				}
+			]
+		}
+	]
 });
