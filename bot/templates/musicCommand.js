4310fc579dd3f4d31bc6ff5535b63878057c9365
KingCh1ll <77991729+KingCh1ll@users.noreply.github.com>
2022-03-30 12:41:13 -0400

    🚀 | Code Formatting

--- bot/templates/musicCommand.js
+++ bot/templates/musicCommand.js
@@ -13,13 +13,8 @@ module.exports = class ModCommand {
 
 		const perms = message.channel.permissionsFor(message.guild.me);
 
-	// 	if (
-	// 		!perms.has(Permissions.FLAGS.SPEAK) ||
-    //   !perms.has(Permissions.FLAGS.CONNECT) ||
-    //   !perms.has(Permissions.FLAGS.USE_VAD)
-	// 	) {
-	// 		return await message.replyT("Make sure I have speak, connect and use voice activity perms.");
-	// 	}
+		// eslint-disable-next-line capitalized-comments
+		// if (!perms.has(Permissions.FLAGS.SPEAK) || !perms.has(Permissions.FLAGS.CONNECT) || !perms.has(Permissions.FLAGS.USE_VAD)) return await message.replyT("Make sure I have speak, connect and use voice activity perms.");
 
 		if (this.execute) {
 			return this.execute(bot, message, args, command, data);
