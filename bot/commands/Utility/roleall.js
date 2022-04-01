8550ba70d01f465ed253dd877ba04333b48d4c09
KingCh1ll <77991729+KingCh1ll@users.noreply.github.com>
2022-03-31 22:32:13 -0400

    🐛 | Bug Fix

--- bot/commands/Utility/roleall.js
+++ bot/commands/Utility/roleall.js
@@ -7,7 +7,7 @@ async function execute(bot, message, args, command, data) {
 	const role = data.options.getRole("role");
 
 	try {
-		await message.guild.members.cache.forEach(async member => await member.roles.add(role));
+		await message.guild.members.cache.forEach(async member => await member.roles.add(role).catch(async err => await message.editT(`Failed to give **${role}**. Please check my permissions and try again.`)));
 
 		await message.editT(`Successfully added the role **${role}** to **${message.guild.members.cache.size}** members.`);
 	} catch (err) {
