54d5029789604002b474974f5f18bdfb623ffe68
KingCh1ll <77991729+KingCh1ll@users.noreply.github.com>
2022-04-05 12:42:15 -0400

    🐛 | Previous Music Bug Fix

--- bot/commands/Music/previous.js
+++ bot/commands/Music/previous.js
@@ -6,6 +6,7 @@ async function execute(bot, message, args, command, data) {
 	const queue = await bot.distube.getQueue(message);
 
 	if (!queue) return await message.replyT(`${bot.config.emojis.error} | No songs in queue.`);
+	if (!queue.previousSongs || queue.previousSongs.length === 0) return await message.replyT(`${bot.config.emojis.error} | There are no previous songs.`);
 
 	const song = queue.previous();
 
@@ -13,9 +14,9 @@ async function execute(bot, message, args, command, data) {
 }
 
 module.exports = new cmd(execute, {
-	description: `Change the current track's position.`,
+	description: "Go back to the song that was playing before the current one.",
 	dirname: __dirname,
-	usage: "<number>",
+	usage: "",
 	aliases: [],
 	perms: [],
 	slash: true
