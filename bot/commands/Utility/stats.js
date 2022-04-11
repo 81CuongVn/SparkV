366432b8a19f6916cd64ff57699d4fe9cb497708
KingCh1ll <77991729+KingCh1ll@users.noreply.github.com>
2022-04-11 18:57:47 -0400

    🪄 | Fixed Stats

--- bot/commands/Utility/stats.js
+++ bot/commands/Utility/stats.js
@@ -31,7 +31,7 @@ module.exports = new cmd(
 				name: (message.user ? message.user : message.author).tag,
 				iconURL: (message.user ? message.user : message.author).displayAvatarURL({ dynamic: true })
 			})
-			.addField(`**${await message.translate("Server")}**`, `${bot.config.emojis.rocket} ${await message.translate("CPU")}: **${(process.cpuUsage().user / 1024 * 2 / 100).toFixed(0)}%**\n${bot.config.emojis.stats} Memory: **${(((os.totalmem() - os.freemem()) / (os.totalmem())) * 100).toFixed(2)}%**`, true)
+			.addField(`**${await message.translate("Server")}**`, `${bot.config.emojis.rocket} ${await message.translate("CPU")}: **${(process.cpuUsage().system / 1000 / 1000 / 10).toFixed(2)}%**`, true)
 			.addField(`**${await message.translate("Bot Statistics")}**`, `${bot.config.emojis.globe} ${await message.translate("Servers")}: **${bot.functions.formatNumber(await bot.functions.GetServerCount())}**\n${bot.config.emojis.player} Users: **${bot.functions.formatNumber(await bot.functions.GetUserCount())}**`, true)
 			.setFooter({
 				text: `SparkV's Stats • ${bot.config.embed.footer}`,
