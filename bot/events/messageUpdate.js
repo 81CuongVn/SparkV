ba6139e8c7654e69b22b752170520a92b42e32d9
KingCh1ll <77991729+KingCh1ll@users.noreply.github.com>
2022-04-19 10:33:02 -0400

    🐛 | Bug Fix

--- bot/events/messageUpdate.js
+++ bot/events/messageUpdate.js
@@ -16,7 +16,7 @@ module.exports = {
 
 		if (!data?.plugins?.logging?.enabled === "true") return;
 
-		const channel = channel?.guild?.channels?.cache.get(data.plugins?.logging?.channel) || await channel?.guild?.channels?.fetch(data.plugins?.welcome?.channel);
+		const channel = newM.channel?.guild?.channels?.cache.get(data.plugins?.logging?.channel) || await newM.channel?.guild?.channels?.fetch(data.plugins?.welcome?.channel);
 
 		if (!channel) return;
 
