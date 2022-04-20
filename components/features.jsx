c9cf27ed2a5e3b73cb1af0b4fc9bd10c84b19b58
KingCh1ll <77991729+KingCh1ll@users.noreply.github.com>
2022-04-20 10:14:48 -0400

    🚀 | Fixed Features - No Closing Tag

--- components/features.jsx
+++ components/features.jsx
@@ -12,8 +12,8 @@ export default function Features({ name, features }) {
           <div class="row flex-lg-row-reverse align-items-center g-5 py-5 px-5 aos-init" data-aos="zoom-in">
             <div class="col-12 col-sm-8 col-lg-6">
               <img defer data-aos="fade-up" src="<%- f.icon %>" class="w-100 mx-lg-auto img-fluid aos-init aos-animate"
-          alt="<%- f.alt %>" style="border-radius:6px;max-width: 400px;">
-            </div>
+          alt="<%- f.alt %>" style="border-radius:6px;max-width: 400px;"/>
+          </div>
             <div class="col-lg-6">
               <h1 class="display-5 text-white fw-bold lh-1 mb-3" data-aos="fade-up">{f.title}</h1>
               <p style="color: #fff9;font-size: 20px;font-family: Satoshi-Regular, sans-serif;" data-aos="fade-up">{f.description}</p>
