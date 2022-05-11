2861638cf7b34604a6d1a53c3ee470ab0ca7bafb
KingCh1ll <77991729+KingCh1ll@users.noreply.github.com>
2022-05-11 15:12:45 -0400

    🐛 | Fixed: Chatbot Cmd Permissions

--- bot/commands/Fun/chatbot.js
+++ bot/commands/Fun/chatbot.js
@@ -14,7 +14,7 @@ module.exports = new cmd(execute, {
 	description: "Talk to me!",
 	dirname: __dirname,
 	usage: "",
-	perms: ["EMBED_LINKS", "MANAGE_MESSAGES"],
+	perms: [],
 	slash: true,
 	slashOnly: true,
 	options: [
