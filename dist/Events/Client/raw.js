e7a520b23a3255549adb657d134b48d4fa186e11
KingCh1ll <77991729+KingCh1ll@users.noreply.github.com>
2022-07-08 14:30:40 -0400

    Delete dist directory

--- dist/Events/Client/raw.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    once: false,
-    async execute(bot, event) {
-        bot.music.updateVoiceState(event);
-    }
-};
-//# sourceMappingURL=raw.js.map
